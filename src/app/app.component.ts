a501de0986cfa60fa4d786ed0aa0980a47fda6a4
Randa10 <randa010011012@gmail.com>
2024-09-02 11:56:42 +0300

    Day1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from './components/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+//meta-data 
+
+@Component({ 
+  selector: 'app-root', 
+  standalone: true,    //v17, v18 
+  imports: [RouterOutlet, NavbarComponent, FooterComponent,HomeComponent,SidebarComponent],  
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Angular-project-frontendG2';
+
+
 }
